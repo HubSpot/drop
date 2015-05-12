b2dd8667928bf9943db27a3a8a59deaed5706822
Nick Hwang <nhwang@hubspot.com>
2015-05-12 19:41:26 -0400

    Prep for compass-free

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var gulp        = require('gulp');
 var babel       = require('gulp-babel');
 var bump        = require('gulp-bump');
 var filter      = require('gulp-filter');
-var sass        = require('gulp-ruby-sass');
 var header      = require('gulp-header');
+var prefixer    = require('gulp-autoprefixer');
 var rename      = require('gulp-rename');
 var uglify      = require('gulp-uglify');
+var sass        = require('gulp-sass');
 var tagVersion  = require('gulp-tag-version');
 var umd         = require('gulp-wrap-umd');
-
 
 // Variables
 var distDir = './dist';
@@ -53,11 +53,12 @@ gulp.task('js', ['clean'], function() {
 
 // CSS
 gulp.task('css', function() {
-  sass('./src/css', {
-    loadPath: './bower_components',
-    compass: true
-  })
-  .pipe(gulp.dest(distDir + '/css'));
+  gulp.src('./src/css/**/*.sass')
+    .pipe(sass({
+      includePaths: ['./bower_components']
+    }))
+    .pipe(prefixer())
+    .pipe(gulp.dest(distDir + '/css'));
 });
 
 
