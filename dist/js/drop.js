HubSpot/drop

commit e609ba0e4765aa66321adc134ab164c39f0f0f4a

Don't let DOM attributes override position, openOn options

dist/js/drop.js
@@ -184,7 +184,7 @@ function createContext() {
       for (var i = 0; i < attrsOverride.length; ++i) {
 
         var override = this.target.getAttribute(dataPrefix + '-' + attrsOverride[i]);
-        if (override) {
+        if (override && this.options[attrsOverride[i]] == null) {
           this.options[attrsOverride[i]] = override;
         }
       }

src/js/drop.js
@@ -143,7 +143,7 @@ function createContext(options={}) {
       for (let i = 0; i < attrsOverride.length; ++i) {
 
         const override = this.target.getAttribute(`${dataPrefix}-${attrsOverride[i]}`);
-        if (override) {
+        if (override && this.options[attrsOverride[i]] == null) {
           this.options[attrsOverride[i]] = override;
         }
       }

commit 385a451cd93da8d673d3ca0fd2b395d5e355e8c5

allow data attributes to override defaults
The commit in e609ba0e4765aa66321adc134ab164c39f0f0f4a broke
the ability to use data attributes to override the position
and onOpen options, since these are always set as defaults.

dist/js/drop.js
@@ -1,4 +1,4 @@
-/*! tether-drop 1.4.1 */
+/*! tether-drop 1.4.2 */
 
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
@@ -184,7 +184,7 @@ function createContext() {
       for (var i = 0; i < attrsOverride.length; ++i) {
 
         var override = this.target.getAttribute(dataPrefix + '-' + attrsOverride[i]);
-        if (override && this.options[attrsOverride[i]] == null) {
+        if (override && (!opts || opts[attrsOverride[i]] == null)) {
           this.options[attrsOverride[i]] = override;
         }
       }

src/js/drop.js
@@ -143,7 +143,7 @@ function createContext(options={}) {
       for (let i = 0; i < attrsOverride.length; ++i) {
 
         const override = this.target.getAttribute(`${dataPrefix}-${attrsOverride[i]}`);
-        if (override && this.options[attrsOverride[i]] == null) {
+        if (override && (!opts || opts[attrsOverride[i]] == null)) {
           this.options[attrsOverride[i]] = override;
         }
       }